a9fccf3ef2d25d230b8317be3ff2120eb2fa999b
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-05-30 18:04:06 +0200

    adds hw05 ready - avatars upload, change, resize to 250px

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const validateBody = require("./validateBody");
 const validateFavorite = require("./validateFavorite");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
+const upload = require ( './upload')
 
 module.exports = {
 	validateBody,
 	validateFavorite,
 	isValidId,
 	authenticate,
+	upload, 
 };
